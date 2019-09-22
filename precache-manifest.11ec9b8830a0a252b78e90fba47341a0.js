393c1d12a7a101cbaf0e5080b65d29b4685f2dec
fahadtanim <fahadtanim@gmail.com>
2019-09-23 02:29:20 +0600

    Updates

---
+++ precache-manifest.11ec9b8830a0a252b78e90fba47341a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f72d1b18bdbde72d9004791fd9a3458",
+    "url": "/blood.donation/index.html"
+  },
+  {
+    "revision": "74ab15847e23fdcc5da5",
+    "url": "/blood.donation/static/css/main.2665bdff.chunk.css"
+  },
+  {
+    "revision": "cf1daf80d6751ca70f13",
+    "url": "/blood.donation/static/js/2.251d33db.chunk.js"
+  },
+  {
+    "revision": "74ab15847e23fdcc5da5",
+    "url": "/blood.donation/static/js/main.c118a833.chunk.js"
+  },
+  {
+    "revision": "293d22af2f61c2da595a",
+    "url": "/blood.donation/static/js/runtime-main.4a79437f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blood.donation/static/media/logo.25bf045c.svg"
+  }
+]);
